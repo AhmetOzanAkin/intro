7d88597e028c853437ea9cc91e5c2a7508ed2f21
AhmetOzanAkin <oznakin@outlook.com>
2024-04-14 18:21:13 +0300

    Proje dosyası ekle.

---
+++ intro/Business/CourseManager.cs
@@ -0,0 +1,40 @@
+﻿using intro.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intro.Business;
+
+public class CourseManager
+{
+    Course[] courses = new Course[3];
+
+    // constructor 
+    public CourseManager()
+    {
+        Course course1 = new Course();
+        course1.Name = "C#";
+        course1.Description = ".NET 8";
+        course1.Price = 0;
+
+        Course course2 = new Course();
+        course2.Name = "Java";
+        course2.Description = "Spring";
+        course2.Price = 10;
+
+        Course course3 = new Course();
+        course3.Name = "Python";
+        course3.Description = "Veri bilimi";
+        course3.Price = 20;
+
+        courses[0] = course1;
+        courses[1] = course2;
+        courses[2] = course3;
+    }
+    public Course[] GetAll()
+    {
+        return courses;
+    }
+}
---
+++ intro/Program.cs
@@ -0,0 +1,103 @@
+﻿// See https://aka.ms/new-console-template for more information
+using intro.Business;
+using intro.Entities;
+
+Console.WriteLine("Hello, World!");
+
+string message = "krediler";
+int term = 12;
+double amount = 1000.5;
+bool isAuthenticated = false;
+
+// variables --> camelCase kullanıyoruz.
+
+Console.WriteLine(message);
+
+if (isAuthenticated)
+{
+    Console.WriteLine("hoşgeldin ozan");
+}
+else
+{
+    Console.WriteLine("giriş yap");
+}
+
+string[] loans = { "Kredi 1", "Kredi 2", "Kredi 3", "Kredi 4", "Kredi 5", "Kredi 6" };
+for (int i = 0; i < loans.Length; i++)
+{
+    Console.WriteLine(loans[i]);
+}
+
+CourseManager courseManager = new CourseManager();
+Course[] courses2 = courseManager.GetAll();
+
+for (int i = 0; i < courses2.Length; i++)
+{
+    Console.WriteLine(courses2[i].Name + " / " + courses2[i].Price);
+}
+
+int gun = 9;
+switch (gun)
+{
+    case 1:
+        Console.WriteLine("Pazartesi");
+        break;
+        case 2:
+        Console.WriteLine("Salı");
+        break;
+        case 3:
+        Console.WriteLine("Çarşamba");
+        break;
+        case 4:
+        Console.WriteLine("Perşembe");
+        break;
+        case 5:
+        Console.WriteLine("Cuma");
+        break;
+        case 6:
+        Console.WriteLine("Cumartesi");
+        break;
+       case 7:
+        Console.WriteLine("Pazar");
+        break;
+        default:
+        Console.WriteLine("Geçersiz gün girdiniz");
+        break;
+}
+
+
+Console.WriteLine("kod bitti");
+
+InvidualCustomer customer1 = new InvidualCustomer();
+customer1.Id = 1;
+customer1.FirstName = "Kodlamaio";
+customer1.LastName = "Akin";
+customer1.NationalIdentity = "123456789";
+customer1.CustomerNumber = "123456";
+
+InvidualCustomer customer2 = new InvidualCustomer();
+customer2.Id = 2;
+customer2.FirstName = "Anatolia";
+customer2.LastName = "Yazilim";
+customer2.NationalIdentity = "12341234";
+customer2.CustomerNumber = "123123";
+
+CorporateCustomer customer3 = new CorporateCustomer();
+customer3.Id = 3;
+customer3.Name = "abc";
+customer3.CustomerNumber = "123231";
+customer3.TaxNumber = "123456";
+
+CorporateCustomer customer4 = new CorporateCustomer();
+customer4.Id = 4;
+customer4.Name = "cba";
+customer4.CustomerNumber = "654321";
+customer4.TaxNumber = "654321";
+
+BaseCustomer[] customers = { customer1, customer2, customer3,customer4 };
+
+// Polymorphism
+foreach (BaseCustomer customer in customers)
+{
+    Console.WriteLine(customer.CustomerNumber);
+}
