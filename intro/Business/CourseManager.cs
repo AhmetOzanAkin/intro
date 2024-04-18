918de51f9599dc362575ee0dd0dc3ba94071b162
AhmetOzanAkin <oznakin@outlook.com>
2024-04-19 00:20:23 +0300

    Day 2 Lesson 2 Commit 1

--- intro/Business/CourseManager.cs
+++ intro/Business/CourseManager.cs
@@ -1,4 +1,5 @@
-﻿using intro.Entities;
+﻿using intro.DataAccess.Concretes;
+using intro.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,32 +10,10 @@ namespace intro.Business;
 
 public class CourseManager
 {
-    Course[] courses = new Course[3];
-
-    // constructor 
-    public CourseManager()
+    public List<Course> GetAll()
     {
-        Course course1 = new Course();
-        course1.Name = "C#";
-        course1.Description = ".NET 8";
-        course1.Price = 0;
-
-        Course course2 = new Course();
-        course2.Name = "Java";
-        course2.Description = "Spring";
-        course2.Price = 10;
-
-        Course course3 = new Course();
-        course3.Name = "Python";
-        course3.Description = "Veri bilimi";
-        course3.Price = 20;
-
-        courses[0] = course1;
-        courses[1] = course2;
-        courses[2] = course3;
-    }
-    public Course[] GetAll()
-    {
-        return courses;
+        //business rules
+        CourseDal courseDal = new CourseDal();
+        return courseDal.GetAll();
     }
 }
---
+++ intro/DataAccess/Concretes/CourseDal.cs
@@ -0,0 +1,41 @@
+﻿using intro.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intro.DataAccess.Concretes;
+
+public class CourseDal
+{
+    List<Course> courses;
+    public CourseDal()
+    {
+        Course course1 = new Course();
+        course1.Name = "C#";
+        course1.Description = ".NET 8";
+        course1.Price = 0;
+
+        Course course2 = new Course();
+        course2.Name = "Java";
+        course2.Description = "Spring";
+        course2.Price = 10;
+
+        Course course3 = new Course();
+        course3.Name = "Python";
+        course3.Description = "Veri bilimi";
+        course3.Price = 20;
+
+        courses = new List<Course> { course1,course2,course3};
+    }
+    public List<Course> GetAll()
+    {
+        //burada database işlemleri yapılacak.
+        return courses;
+    }
+    public void Add(Course course)
+    {
+        courses.Add(course);
+    }
+}
